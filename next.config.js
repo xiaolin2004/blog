9783c98a5d60d76812310e2a4b97b518f5f460a5
xiaolin2004 <1553367438@qq.com>
2024-12-29 09:32:50 +0800

    重构 Next.js 配置，删除旧配置文件并更新输出目录为 dist

--- next.config.js
+++ next.config.js
@@ -11,7 +11,10 @@ const nextConfig = {
   // skipTrailingSlashRedirect: true,
 
   // Optional: Change the output directory `out` -> `dist`
-  distDir: 'dist',
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
